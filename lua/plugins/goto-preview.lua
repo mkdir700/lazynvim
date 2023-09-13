c4a1c5120a4adac9a2a0dc220212574946ac36f3
mkdir700 <mkdir700@gmail.com>
2023-09-13 20:45:33 +0800

    add plugin goto-preview

---
+++ lua/plugins/goto-preview.lua
@@ -0,0 +1,9 @@
+return {
+  "rmagatti/goto-preview",
+  keys = {
+    { "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>" },
+  },
+  config = function()
+    require("goto-preview").setup({})
+  end,
+}
