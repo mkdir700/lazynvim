caf1465d56ee84735a29063f052f4c56f44dc08e
YuhangWu <yuhang.wu@xyzrobotics.ai>
2023-09-14 15:53:23 +0800

    使用 nvim-osc52 插件

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,11 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-
 -- Disable autoformat for lua files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "lua" },
   callback = function()
     vim.b.autoformat = false
+  end,
+})
+
+-- Copy to system clipboard on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+      require("osc52").copy_register("+")
+    end
   end,
 })
