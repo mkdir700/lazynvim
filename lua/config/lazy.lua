15ceaa2764a5dff6ed2fe312341dcc8942c686ab
mkdir700 <mkdir700@gmail.com>
2023-09-24 15:53:59 +0800

    add plugin go.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    -- { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
