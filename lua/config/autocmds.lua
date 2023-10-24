6275cc547ee692b57283a44fc828fc23d1c97263
mkdir700 <mkdir700@gmail.com>
2023-10-24 09:16:51 +0800

    自动激活 python 虚拟环境

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,3 +31,16 @@ vim.api.nvim_create_autocmd({ "User" }, {
     vim.api.nvim_command("cd " .. root_dir)
   end,
 })
+
+-- Auto select virtualenv on Nvim open if pyproject.toml is present
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
+  end,
+  once = true,
+})
