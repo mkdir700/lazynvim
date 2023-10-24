9084cd45c9104bc09385f2b1f46b2b417a24b64d
mkdir700 <mkdir700@gmail.com>
2023-10-24 16:38:59 +0800

    bufferline 新增关闭其他 buffer 的快捷键

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    -- {
+    --   "<leader>bc",
+    --   ":BufferLinePick<CR>",
+    --   desc = "Pick buffer from list",
+    -- },
+    {
+      "<leader>bo",
+      ":BufferLineCloseOthers<CR>",
+      desc = "Close all other buffers",
+    },
+  },
+}
