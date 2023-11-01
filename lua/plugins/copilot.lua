92c76381b246e84a485d72e272a5760d8a283b81
mkdir700 <mkdir700@gmail.com>
2023-11-01 14:48:12 +0800

    remove plugin copilot.cmp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,16 @@
 return {
   "zbirenbaum/copilot.lua",
+  event = "VeryLazy",
   cmd = "Copilot",
   build = ":Copilot auth",
   opts = {
-    suggestion = { enabled = true },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept_word = "<M-y>",
+      },
+    },
     panel = { enabled = false },
     filetypes = {
       markdown = true,
