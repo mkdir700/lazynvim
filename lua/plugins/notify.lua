86dad7b2834a6c719bfb553d5b995ddf92e04057
mkdir700 <mkdir700@gmail.com>
2024-01-10 09:43:28 +0800

    修改 notify 配置
    减少 timeout
    使用 slide 作为进出动画

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,11 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    --- @usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
+    stages = "slide",
+    --- @usage timeout for notification in ms, default 5000
+    timeout = 2000,
+    --- @usage User render fps value
+    fps = 24,
+  },
+}
