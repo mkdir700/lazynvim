a8d73aa1853c94b225e768d752344f5401d1a8a2
mkdir700 <mkdir700@gmail.com>
2024-01-16 15:25:08 +0800

    Update conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,12 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  event = "BufWritePre",
+  optional = true,
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
       go = { "goimports", "gofmt" },
-      python = function(bufnr)
-        if require("conform").get_formatter_info("ruff_format", bufnr).available then
-          return { "ruff_format" }
-        else
-          return { "isort", "black" }
-        end
-      end, -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" }, -- Conform will run multiple formatters sequentially
       -- Use a sub-list to run only the first available formatter
       javascript = { { "prettierd", "prettier" } },
     },
