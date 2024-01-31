8b8e68244f79b3bd8f6cc0234f580502a38f0438
mkdir700 <mkdir700@gmail.com>
2024-01-31 17:56:55 +0800

    Update notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,9 +2,9 @@ return {
   "rcarriga/nvim-notify",
   opts = {
     --- @usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
-    stages = "slide",
+    stages = "fade",
     --- @usage timeout for notification in ms, default 5000
-    timeout = 2000,
+    timeout = 1000,
     --- @usage User render fps value
     fps = 24,
   },
