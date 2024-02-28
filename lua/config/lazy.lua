e595a800e9e86371949492747b11a4509b50d4b7
mkdir700 <mkdir700@gmail.com>
2024-02-28 09:44:04 +0800

    disable python-semshi

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,8 +24,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    -- Highlighting for Python
-    { import = "lazyvim.plugins.extras.lang.python-semshi" },
     -- import/override with your plugins
     { import = "plugins" },
   },
