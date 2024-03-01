7658d2e7d45998616e92a7015eaf4de76778f55e
mkdir700 <mkdir700@gmail.com>
2024-03-01 11:32:52 +0800

    移除 persisted

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,5 @@
 local disabled_plugins = {
   "echasnovski/mini.surround",
-  -- "folke/persistence.nvim",
-  "olimorris/persisted.nvim",
   "echasnovski/mini.pairs",
   "s1n7ax/nvim-window-picker"
 }
