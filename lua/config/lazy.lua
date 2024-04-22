891272a37a9323cbf5a2f0c73ceb2487ca28ceb9
mkdir700 <mkdir700@gmail.com>
2024-04-22 21:33:45 +0800

    Add outline plugin to lazy.lua configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
     -- import/override with your plugins
     { import = "plugins" },
   },
