c4bfd96ecf3181dffc788c5ea1aba39694d06e87
mkdir700 <mkdir700@gmail.com>
2024-06-29 17:09:20 +0800

    升级 rainbow 插件

--- lua/plugins/nvim-ts-rainbow.lua
+++ lua/plugins/nvim-ts-rainbow.lua
@@ -1,17 +1,7 @@
 return {
-  "HiPhish/nvim-ts-rainbow2",
+  "hiphish/rainbow-delimiters.nvim",
   event = "BufRead",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      rainbow = {
-        enable = true,
-        -- list of languages you want to disable the plugin for
-        disable = { "jsx", "cpp" },
-        -- Which query to use for finding delimiters
-        query = "rainbow-parens",
-        -- Highlight the entire buffer all at once
-        strategy = require("ts-rainbow").strategy.global,
-      },
-    })
+    require("nvim-treesitter.configs").setup({})
   end,
 }
