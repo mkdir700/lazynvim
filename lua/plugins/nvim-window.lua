95d1a0a21ee90b7aa2e697443a7ddba7e67ba417
mkdir700 <mkdir700@gmail.com>
2024-07-17 20:19:46 +0800

    修改 nvim-windows 的热键

--- lua/plugins/nvim-window.lua
+++ lua/plugins/nvim-window.lua
@@ -1,7 +1,7 @@
 return {
   "yorickpeterse/nvim-window",
   keys = {
-    { "<leader>wj", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
+    { "<leader>wJ", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
     { "<M-w>", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
   },
   config = true,
