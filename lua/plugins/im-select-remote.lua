30557cc763dce720f80fc9ae147e470412ccec9b
mkdir700 <mkdir700@gmail.com>
2024-07-18 10:41:16 +0800

    根据 ssh 是否连接选择用于自动切换输入法的插件

--- lua/plugins/im-select-remote.lua
+++ lua/plugins/im-select-remote.lua
@@ -1,12 +1,33 @@
 return {
-  "mkdir700/im-select-remote.nvim",
-  config = function()
-    require("im-select-remote").setup({
-      config = {
-        socket = {
-          command = "im-select -",
+  -- for local machine
+  {
+    "keaising/im-select.nvim",
+    cond = function()
+      -- 检查环境变量 SSH_TTY 是否存在
+      return not os.getenv("SSH_TTY")
+    end,
+    config = function()
+      require("im_select").setup({
+        default_im_select = "keyboard-us",
+        default_command = "fcitx5-remote",
+      })
+    end,
+  },
+  -- for ssh
+  {
+    "mkdir700/im-select-remote.nvim",
+    cond = function()
+      -- 检查环境变量 SSH_TTY 是否存在
+      return os.getenv("SSH_TTY")
+    end,
+    config = function()
+      require("im-select-remote").setup({
+        config = {
+          socket = {
+            command = "im-select -",
+          },
         },
-      },
-    })
-  end,
+      })
+    end,
+  },
 }
