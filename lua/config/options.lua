c20732035d59fe0ac826e7f66d6c593d45732827
mkdir700 <mkdir700@gmail.com>
2024-07-25 20:22:31 +0800

    配置 neovide 样式

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,21 @@ vim.o.swapfile = false
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+
+-- neovide options
+vim.g.neovide_cursor_animation_length = 0.03
+vim.g.neovide_cursor_trail_length = 0.2
+vim.g.neovide_cursor_antialiasing = true
+vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_mode = "ripple"
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_unfocused_outline_width = 0.125
+
+-- neovide - window
+vim.g.neovide_window_blurred = true
+-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+vim.g.neovide_transparency = 0.9
+-- vim.g.transparency = 0.8
+-- vim.g.neovide_background_color = "#0f1117" .. alpha()
+vim.g.experimental_layer_grouping = false
+
