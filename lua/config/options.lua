7fc82a52a907e2ac6569822380eb8902f967a313
mkdir700 <mkdir700@gmail.com>
2025-04-29 13:54:20 +0800

    feat: Add a new plugin `ufo`

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.wo.foldmethod = "expr"
--- vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
--- vim.wo.foldlevel = 10
--- vim.wo.foldminlines = 1
--- vim.wo.foldnestmax = 10
--- vim.wo.foldtext =
---   [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 vim.o.sessionoptions = "buffers,curdir,folds,globals,tabpages,winpos,winsize"
 vim.o.swapfile = false
