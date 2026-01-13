7d03d9651865d172942bd134b1639e696936064e
mkdir700 <mkdir700@gmail.com>
2026-01-13 20:30:23 +0800

    set lazy to false for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return {
   "sindrets/diffview.nvim",
+  lazy = false,
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>ad", "<CMD>DiffviewOpen<CR>", desc = "Open Diffview" },
