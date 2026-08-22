33ba4c646ac586f4aa6decce900628f784cb5615
mkdir700 <mkdir700@gmail.com>
2026-08-22 17:04:43 +0800

    fix: preserve undo across autoformat

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,12 +3,14 @@ return {
   event = "BufWritePre",
   optional = true,
   opts = {
+    default_format_opts = {
+      undojoin = true,
+    },
     formatters_by_ft = {
       lua = { "stylua" },
       go = { "goimports", "gofmt" },
       python = { "ruff_fix", "ruff_organize_imports", "ruff_format" }, -- Conform will run multiple formatters sequentially
-      -- Use a sub-list to run only the first available formatter
-      javascript = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
     },
   },
 }
